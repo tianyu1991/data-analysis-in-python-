61e51c9dcc30a1847bffaa6de33d552c784e3760
tianyu liu <hulan1991@gmail.com>
2016-04-16 03:35:19 -0400

    Update code.py

--- code.py
+++ code.py
@@ -1,5 +1,7 @@
 from pandas import Series, DataFrame
 import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
 
 data_2011= pd.read_csv('C:/Users/tianyu/Desktop/0415/Calls_for_Service_2011.csv')
 data_2012= pd.read_csv('C:/Users/tianyu\Desktop/0415/Calls_for_Service_2012.csv')
@@ -18,3 +20,12 @@ print float(type_counts[1])/data_sum['Type_'].count()
 ##Some calls result in an officer being dispatched to the scene, 
 ##and some log an arrival time. What is the median response time 
 ##(dispatch to arrival), in seconds, considering only valid (i.e. non-negative) times?
+subdata=data_sum[['TimeDispatch','TimeArrive']]
+subdata2=subdata[pd.notnull(subdata['TimeArrive'])&pd.notnull(subdata['TimeDispatch'])]
+arrive_time=map(lambda x:datetime.strptime(x, '%m/%d/%Y %I:%M:%S %p'),subdata2['TimeArrive'])
+dispatch_time=map(lambda x:datetime.strptime(x, '%m/%d/%Y %I:%M:%S %p'),subdata2['TimeDispatch'])
+delta=map(lambda x,y:x-y, arrive_time, dispatch_time)
+delta2=map(lambda x:x.seconds,delta)
+delta3=delta2[delta2>=0]
+np.median(delta3)
+##1254.0
